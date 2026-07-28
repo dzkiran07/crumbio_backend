cd07969b4addb8f25684e7ea79aeb6f9fdc1ba1c
dzkiran07 <kasalawotkiran2@gmail.com>
2026-07-28 17:49:05 +0545

    remove Esewa payment methods and related validation functions

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,11 +1,5 @@
 import { Request, Response } from "express";
-import {
-  validateInitiateEsewaPaymentDto,
-  validateInitiateKhaltiPaymentDto,
-  validateVerifyEsewaPaymentDto,
-  validateVerifyKhaltiPaymentDto,
-} from "../dtos/payment.dto";
-import { esewaService } from "../services/esewa.service";
+import { validateInitiateKhaltiPaymentDto, validateVerifyKhaltiPaymentDto } from "../dtos/payment.dto";
 import { khaltiService } from "../services/khalti.service";
 import { asyncHandler } from "../utils/async-handler";
 
@@ -18,17 +12,5 @@ export const initiateKhalti = asyncHandler(async (req: Request, res: Response) =
 export const verifyKhalti = asyncHandler(async (req: Request, res: Response) => {
   const dto = validateVerifyKhaltiPaymentDto(req.body);
   const result = await khaltiService.verify(dto.pidx);
-  res.status(200).json(result);
-});
-
-export const initiateEsewa = asyncHandler(async (req: Request, res: Response) => {
-  const dto = validateInitiateEsewaPaymentDto(req.body);
-  const result = await esewaService.initiate(dto.orderId);
-  res.status(200).json(result);
-});
-
-export const verifyEsewa = asyncHandler(async (req: Request, res: Response) => {
-  const dto = validateVerifyEsewaPaymentDto(req.body);
-  const result = await esewaService.verify(dto.orderId, dto.transactionUuid, dto.totalAmount);
   res.status(200).json(result);
 });
